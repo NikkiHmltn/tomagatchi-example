7e3c38762c2428e315dc0625de81526c37207300
NikkiHmltn <nikkihmltn1@yahoo.com>
2023-01-19 11:41:36 -0800

    got timers for status bars working along with some css

--- js/app.js
+++ js/app.js
@@ -13,10 +13,13 @@ let shipAudio = document.getElementById('shipSound')
 let supplyBar = document.getElementById('supply-stat')
 let restBar = document.getElementById('rest-stat')
 let funBar = document.getElementById('entertainment-stat')
+let supplyHealth = document.getElementById('supply-bar')
+let restHealth = document.getElementById('rest-bar')
+let funHealth = document.getElementById('entertainment-bar')
 
 
 class TomaShip {
-    //Ship has 3 types of resources
+    //Ship has 3 types of "resources"
     constructor(name, age=0, supplies=30, rest=20, entertainment=25){
         this.name = name
         this.age = age //max 120
@@ -24,6 +27,9 @@ class TomaShip {
         this.rest = rest //max 20
         this.entertainment = entertainment //max 25
         this.gameover = false;
+        this.supplyTimer;
+        this.restTimer;
+        this.funTimer;
     }
 
     setName(input){
@@ -62,22 +68,50 @@ class TomaShip {
     startTimers(){
         //user activates this onClick of 'start' button after startGame runs (ideally)
         //Starts the timers for all 4 resources (2 minutes each)
+        this.supplyTimer = setInterval(()=>{
+            this.supplies -= 3
+        }, 1000)
+        this.restTimer = setInterval(()=>{
+            this.rest -=4
+
+        }, 2000)
+        this.funTimer = setInterval(()=>{
+            this.entertainment -= 2
+
+        }, 3500)
     }
 
     startGame(){
         let shipName = prompt("What is the name of your ship, Captain?")
-        this.setName(shipName)
+        this.setName(shipName)   
+        this.startTimers()   
+    }
+
+    endGame(timerInt){
+        if(this.gameover === true){
+            clearInterval(timerInt)
+        }
     }
 
     displayVitals(){
-        supplyBar.innerText = this.supplies
-        restBar.innerText = this.rest
-        funBar.innerText = this.entertainment
+        supplyHealth.innerText = this.supplies
+        supplyHealth.style.width = `${Math.floor(this.supplies/30*100)}%`
+        
+        restHealth.innerText = this.rest
+        restHealth.style.width = `${Math.floor(this.rest/20*100)}%`
+
+        funHealth.innerText = this.entertainment
+        funHealth.style.width = `${Math.floor(this.entertainment/25*100)}%`
+
     }
 
     checkLoss(){
         //If entertainment|sleep|supply === 0, GAME OVER
         //If age === 120, GAME OVER
+        if(this.entertainment <= 0 || this.rest <= 0 || this.supplies <= 0){
+            console.log("GAME OVER")
+            return true
+        }
     }
 
     animateStatInc(){
@@ -99,15 +133,39 @@ class TomaShip {
 let ship = new TomaShip()
 ship.startGame()
 
-// let timer = 0
+let timer = 0
 //SET INTERVAL HERE TO START THE GAME
-// setInterval(() => {
-//     timer += 1
-//     console.log(timer)
+const timerInt = setInterval(() => {
+    timer += 1
+    // console.log(timer)
     
-// },1000)
+},1000)
 
-//if timer is 120 //clearInterval() and gameends if no winner
+//CONSTANTLY CHECKS FOR GAME STATUS UPDATES
+const checkGameStats = setInterval(()=>{
+    ship.displayVitals()
+    if(timer >= 120){
+        //stop the game
+        ship.gameover = true;
+        ship.endGame(timerInt)
+        ship.endGame(checkGameStats)
+        ship.endGame(ship.supplyTimer)
+        ship.endGame(ship.restTimer)
+        ship.endGame(ship.funTimer)
+
+    }
+    if(ship.checkLoss()){
+        ship.gameover = true
+        ship.endGame(timerInt)
+        ship.endGame(checkGameStats)
+        ship.endGame(ship.supplyTimer)
+        ship.endGame(ship.restTimer)
+        ship.endGame(ship.funTimer)
+    }
+},100)
+
+
+// if timer is 120 //clearInterval() and gameends if no winner
 
 // DOM EVENTS // 
 supplyBtn.addEventListener('click', ship.addSupplies.bind(ship))
